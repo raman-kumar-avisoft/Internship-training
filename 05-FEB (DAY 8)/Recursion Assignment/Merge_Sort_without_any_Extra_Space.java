0a823595d6fff2101de1b7491621a7122ae6f873
raman-kumar-avisoft <158030280+raman-kumar-avisoft@users.noreply.github.com>
2024-02-07 10:31:46 +0530

    Merge_Sort_without_any_Extra_Space.java

--- 05-FEB (DAY 8)/Recursion Assignment/Merge_Sort_without_any_Extra_Space.java
+++ 05-FEB (DAY 8)/Recursion Assignment/Merge_Sort_without_any_Extra_Space.java
@@ -4,8 +4,8 @@ public class Merge_Sort_without_any_Extra_Space {
     static void merge(int[] arr, int s, int mid, int e){
         int i = s;
         int j = mid+1;
-//        int k = s;
-        while(i < j && j < e){
+//        having some issues figuring out condition i< j : dry run this for better understanding 1 5 3 4 26 2 8
+        while(i < j && j <= e){
             if(arr[j] < arr[i]){
                 int elm = arr[j];
 //                shifting
@@ -20,17 +20,22 @@ public class Merge_Sort_without_any_Extra_Space {
                 i++;
             }
         }
-//        while(i<=mid)
     }
     static void solve(int[] arr, int s, int e){
 //        base condition
         if(s>=e){
             return;
         }
+
         int mid = (e-s)/2 + s;
+
         solve(arr, s, mid);
         solve(arr, mid+1, e);
         merge(arr, s, mid, e);
+        System.out.println();
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -43,11 +48,16 @@ public class Merge_Sort_without_any_Extra_Space {
             arr[i] = sc.nextInt();
         }
 
+        System.out.println("Array before merge sort is: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+
         solve(arr, 0, arr.length-1);
 
-        System.out.println("Elements in array after merge sort are: ");
+        System.out.println("\nElements in array after merge sort are: ");
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+ " ");
         }
     }
 }
