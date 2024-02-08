49a68d5feaf2514c06ddc09dfec486de3d1b986b
raman-kumar-avisoft <158030280+raman-kumar-avisoft@users.noreply.github.com>
2024-02-09 01:37:08 +0530

    Check_weather_all_word_list_can_be_made_from_inpString.java

--- 07-FEB (DAY 10)/Assignment 2/Check_weather_all_word_list_can_be_made_from_inpString.java
+++ 07-FEB (DAY 10)/Assignment 2/Check_weather_all_word_list_can_be_made_from_inpString.java
@@ -15,10 +15,21 @@ public class Check_weather_all_word_list_can_be_made_from_inpString {
         return solve(str,inp,++i);
     }
     public static void main(String[] args) {
-        String inpStr = "jammukashmirjammu";
-        String[] wordsList = new String[]{"jammu", "kashmir"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input String: ");
+        String inpStr = sc.next();
 
-        boolean ans = solve(inpStr, wordsList, 0);
+        System.out.println("Enter the size of the wordList");
+        int len = sc.nextInt();
+
+        String[] arr = new String[len];
+        System.out.println("Enter the words in wordList");
+        for(int i=0; i<len; i++){
+            arr[i] = sc.next();
+            sc.nextLine();
+        }
+
+        boolean ans = solve(inpStr, arr, 0);
         System.out.println("Answer is: "+ans);
     }
 }
