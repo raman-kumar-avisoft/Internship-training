f6f2e77ef42c85c12e0d6fd3608daa490691a128
raman-kumar-avisoft <158030280+raman-kumar-avisoft@users.noreply.github.com>
2024-03-11 16:51:24 +0530

    callRecords.sql

--- 07-MAR (DAY 31)/callRecords.sql
+++ 07-MAR (DAY 31)/callRecords.sql
@@ -69,12 +69,11 @@ INSERT INTO Calls (from_id, to_id, duration) VALUES
 (4, 3, 499);
 
 -- select  to report the number of calls and the total call duration between each pair of distinct persons
+
 SELECT 
-    CASE WHEN from_id < to_id THEN from_id ELSE to_id END AS person1,
-    CASE WHEN from_id < to_id THEN to_id ELSE from_id END AS person2,
+    LEAST(from_id, to_id) AS person1,
+    GREATEST(from_id, to_id) AS person2,
     COUNT(*) AS call_count,
     SUM(duration) AS total_duration
 FROM Calls
-GROUP BY 
-    CASE WHEN from_id < to_id THEN from_id ELSE to_id END,
-    CASE WHEN from_id < to_id THEN to_id ELSE from_id END;
+GROUP BY person1, person2;
