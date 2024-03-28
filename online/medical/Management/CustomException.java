9192af7921eec2cc1630a58158330587acb6bc58
raman-kumar-avisoft <158030280+raman-kumar-avisoft@users.noreply.github.com>
2024-03-28 22:10:33 +0530

    Delete online directory

--- online/medical/Management/CustomException.java
+++
@@ -1,7 +0,0 @@
-package online.medical.Management;
-
-public class CustomException extends Exception{
-    public CustomException(String error){
-        super(error);
-    }
-}
--- online/voting/system/CustomException.java
+++
@@ -1,7 +0,0 @@
-package online.voting.system;
-
-public class CustomException extends Exception{
-    public CustomException(String errorMessage) {
-        super(errorMessage);
-    }
-}
